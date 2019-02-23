c79df98adbb8dc4e96f81afd6e94b954d651479e
Gourab Chanda <gourab.chanda@unilever.com>
2019-02-23 15:20:14 +0530

    This is third commit for reading inpout file

--- sna_script.r
+++ sna_script.r
@@ -1,8 +1,9 @@
 #load the required packages into the R Script 
-packages.req <- c("igraph","sna","dplyr","stringr","dplyr","ggplot2","network")
+packages.req <- c("igraph","sna","dplyr","stringr","dplyr","ggplot2","network","reader")
 packages.diff <- setdiff(packages.req,rownames(installed.packages()))
 if(length(packages.diff)>0){
   install.packages(packages.diff)
 }
 invisible(sapply(packages.req,library,character.only=T))
 #read the input file into the script
+input.data <- read.csv("https://raw.githubusercontent.com/OPER682-Tucker/Social-Network-Analysis/master/Actors.csv") 
